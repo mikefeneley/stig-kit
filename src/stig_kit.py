cb5dd7f8e8fb0fed5de4384bd8d71846b1b670c3
Michael <michael.j.feneley@gmail.com>
2016-10-31 16:08:04 -0400

    Generic commit

---
+++ src/stig_kit.py
@@ -0,0 +1,16 @@
+class STIG_KIT:
+    def __init__(self):
+        print("Init")
+    
+    def start(self):
+        print("Hello")
+
+if __name__ == "__main__":
+    stig_kit = STIG_KIT()
+    stig_kit.start()
+
+
+
+
+
+
